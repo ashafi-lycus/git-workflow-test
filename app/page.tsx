541e8614d04e44405bbb704a5d876fff3372e41c
ashafi-lycus <adnan@lycusinc.com>
2025-10-08 10:37:07 +0500

    Add Prettier configuration and implement CPU heavy task in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
+  function cpuHeavyTask() {
+    const start = Date.now();
+    while (Date.now() - start < 30000) {
+      Math.sqrt(Math.random());
+    }
+  }
+  cpuHeavyTask();
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
