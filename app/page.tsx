2c5b20084528d2bd6e6501075870dbb5b36d6677
ashafi-lycus <adnan@lycusinc.com>
2025-10-08 11:54:13 +0500

    Update footer link text for clarity and completeness

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Examples you can see
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
